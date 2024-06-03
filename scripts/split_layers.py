5a3cbd80216e538185d4601a27a7a07440e91787
ziul-bio <ziul.bio@gmail.com>
2024-06-03 18:10:00 -0500

    clean folder

--- scripts/split_layers.py
+++ scripts/split_layers.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3 -u
 
+# how to run:
+# python scripts/split_layers.py -I embeddings/data_set_AllLayers_mean -O embeddings/data_set_AllLayers_splited
+
+
 import os
 import torch
+import argparse
 
 def save_embeddings_per_layer(embed_path, new_path):
     count = 0
@@ -9,15 +14,16 @@ def save_embeddings_per_layer(embed_path, new_path):
         try:
             if file.endswith('.pt'):
                 count += 1
-                print(f'Splitting file number {count}: {file}')
-
                 file_path = os.path.join(embed_path, file)
                 tensor = torch.load(file_path)
                 num_layers = len(tensor['mean_representations'])
 
+                if count % 1000 == 0:
+                    print(f'Processed {count} files')
+
                 for layer in range(num_layers):
-                    # Create a directory for the current layer if it doesn't exist
                     layer_dir = os.path.join(new_path, str(layer))
+                    # Create a directory for the current layer if it doesn't exist
                     if not os.path.exists(layer_dir):
                         os.makedirs(layer_dir)
 
@@ -32,8 +38,15 @@ def save_embeddings_per_layer(embed_path, new_path):
             print(f"Error processing file {file}: {e}")
 
 if __name__ == "__main__":
-    embed_path = "/stor/work/Wilke/luiz/tail_stability/embeddings/tail_stability_esm2_15B_mean/"
-    new_path = "/stor/work/Wilke/luiz/tail_stability/embeddings/esm2_15B_embeds_per_layer/"
 
-    save_embeddings_per_layer(embed_path, new_path)
+    parser = argparse.ArgumentParser(description='Split embeddings per layer')
+    parser.add_argument('-I', '--embed_path', type=str, help='Path to the embeddings to split')
+    parser.add_argument('-O', '--out_path', type=str, help='Path to save the split embeddings')
+
+    args = parser.parse_args()
+    embed_path = args.embed_path
+    out_path = args.out_path
+    os.makedirs(out_path, exist_ok=True)
+
+    save_embeddings_per_layer(embed_path, out_path)
 
